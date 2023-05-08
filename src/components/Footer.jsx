a15e713c832ea33317e615011caa528332eb123e
Jonassons <albin.jonasson00@outlook.com>
2023-05-08 15:04:35 +0200

    new build x2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import "../css/Footer.css";
 export default function Footer() {
   return (
     <div className="footer">
-      <p>Customer support : wedontcare@pissOff.com</p>
+      <p>Customer support : wedontcare@pissOff.net</p>
     </div>
   );
 }
